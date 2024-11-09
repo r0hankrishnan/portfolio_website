49cd4d9a1925c6d9790d940835d709e8a37b7974
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-11-08 21:41:45 -0500

    change toggleMenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
+
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+  if(icon.classList.contains('open')) {
+    icon.setAttribute('aria-pressed', 'true')
+  } else {
+    icon.setAttribute('aria-pressed', 'false')
   }
+}
